b63a3852fdd3fc40f9a656936eb037a279367ab0
HanaaChouiter <hanaa7chouiter@gmail.com>
2021-09-28 17:23:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+var fruits = ["mango", "lemon" ,"blueberry"]
+//console.log(fruits)
+//console.table(fruits)
+
+var ingredients = ["eggs", "milk" ,"butter"]
+//console.log(ingredients[1])
+//console.log(ingredients.indexOf("butter"))
+
+var objects = ["pen", "book", "lamp"]
+
+objects.unshift("chair");
+//console.log(objects)
+
+objects.pop();
+//console.log(objects)
+
+objects.push("laptop");
+//console.log(objects)
+
+objects.shift();
+//console.log(objects)
+
+var numbers = [4, 10, 8, 12 , 6]
+
+//console.log(numbers.reverse())
+//console.log(numbers.sort((a, b) => a - b))
+
+var total = 0
+var limit = 10
+
+for ( i = 1; i <= limit ; i++){
+    total = total + i;
+    //console.log(i)
+}
+//console.log(total)
+
+var sentence = "Hello Konexio !"
+var sentenceInverse=""
+
+for ( i=0; i <= sentence.length - 1 ; i++){
+   sentenceInverse= sentence[i] + sentenceInverse;
+   console.log(i)
+}
+console.log(sentenceInverse)
+
+
+    for (i=0; i<=100; i++){
+        if( i % 3 === 0){
+            //console.log("fizz ")
+        }else if( i % 5 === 0){
+            //console.log("buzz ")
+        } if(i % 3 && i % 5 === 0){
+            //console.log("fizzbuzz ")
+        }if (i % 7 === 0){
+        }else{
+            //console.log(i)
+        }
+    }
+
+while( total < limit) {
+    total = total + i;
+    //console.log(i)
+}
+//console.log(total)
+
+var camarades = ["Salwa", "Jong","Marco","Jeremy","Helene","Alin","Evan","Kevin","Eloi" ]
+
+for (i=0; i<=camarades.length; i++){
+    team= camarades[Math.floor(Math.random()*camarades.length)]
+}
+//console.log(team)
+
+var tableau = ""
+
+
+// for (i=0; i<=100; i++){
+//    
+// }
+
+
