5816e57ecf7bb06f260d9412e62b793d83b5e28f
HanaaChouiter <hanaa7chouiter@gmail.com>
2021-09-28 19:38:08 +0200

    modification dernier bonus

--- index.js
+++ index.js
@@ -37,19 +37,19 @@ for ( i = 1; i <= limit ; i++){
 var sentence = "Hello Konexio !"
 var sentenceInverse=""
 
-for ( i=0; i <= sentence.length - 1 ; i++){
-   sentenceInverse= sentence[i] + sentenceInverse;
-   console.log(i)
-}
-console.log(sentenceInverse)
+// for ( i=0; i <= sentence.length - 1 ; i++){
+//    sentenceInverse= sentence[i] + sentenceInverse;
+//    //console.log(i)
+// }
+// //console.log(sentenceInverse)
 
 
     for (i=0; i<=100; i++){
         if( i % 3 === 0){
             //console.log("fizz ")
-        }else if( i % 5 === 0){
+        }if( i % 5 === 0){
             //console.log("buzz ")
-        } if(i % 3 && i % 5 === 0){
+        } if(i % 3 === 0 && i % 5 === 0){
             //console.log("fizzbuzz ")
         }if (i % 7 === 0){
         }else{
@@ -70,11 +70,24 @@ for (i=0; i<=camarades.length; i++){
 }
 //console.log(team)
 
-var tableau = ""
+var tableau = []
+var min = 0;
+var max = 100;
 
+var entierplusgrand = 0
 
-// for (i=0; i<=100; i++){
-//    
-// }
+for (i=0; i<20; i++){
+   sort=Math.floor(Math.random() * (max - min + 1) + min)
+   tableau.push(sort)
+   for(i=0; i<=tableau.length; i++){
+        if(tableau[i]> entierplusgrand){
+            entierplusgrand=tableau[i]
+        }
+   }
+   
+}
+console.table(tableau)
+console.table(entierplusgrand)
+
 
 
